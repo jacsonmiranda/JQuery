d3237c03544cba47c42d6393e93c4b5b08366338
Jacson Miranda <jacsonmiranda@gmail.com>
2020-07-13 23:40:45 -0300

    js code, click function and change events

--- 03-02-event-delegation/js/script.js
+++ 03-02-event-delegation/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('#example').on('click', 'button.switch', function() {
+		console.log(this);
 		console.log($(this));
 
 		$(this).parent().toggleClass('highlighted');
