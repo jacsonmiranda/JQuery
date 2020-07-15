fd63fe130916dcb417fc408a184af76d4bde467b
Jacson Miranda <jacsonmiranda@gmail.com>
2020-07-14 23:24:22 -0300

    use of change - select

--- 03-03-event-select/js/script.js
+++ 03-03-event-select/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-	$('#example').on('click', 'button', function(){
-		var selected = $('#place option:selected');
+	$('#example').on('change', 'select', function(){
+		var selected = $(this).find('option:selected');
+
 		var value = selected.val();
 		//var price = selected.attr('data-price');
 		var price = selected.data('price');
@@ -12,6 +13,22 @@ $(document).ready(function() {
 		if(price) {
 			$('#result').html(value + ' ' + price);
 		}
-		
+
 	});
+	// $('#example').on('click', 'button', function(){
+	// 	var selected = $('#place option:selected');
+	//
+	// 	var value = selected.val();
+	// 	//var price = selected.attr('data-price');
+	// 	var price = selected.data('price');
+	//
+	// 	console.log(value);
+	// 	console.log(price);
+	// 	console.log(selected.attr('data-price'));
+	//
+	// 	if(price) {
+	// 		$('#result').html(value + ' ' + price);
+	// 	}
+	//
+	// });
 });
