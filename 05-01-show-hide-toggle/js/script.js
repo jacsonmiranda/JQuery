65365813c344ddf7bd92470ae2df50131c657b10
Jacson Miranda <jacsonmiranda@gmail.com>
2020-07-16 10:44:22 -0300

    use of hide, show, toggle and slideToggle

---
+++ 05-01-show-hide-toggle/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('.item-box').on('click', 'a.info-link', function(event){
+		event.preventDefault();
+
+		//hide(), toggle()
+		//$(this).closest('.item-box').find('.more-info').toggle('slow', function(){
+		//	alert('finished');
+		//});
+
+		$(this).closest('.item-box').find('.more-info') .slideToggle('fast');
+	});
+});
