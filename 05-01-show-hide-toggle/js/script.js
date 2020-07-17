28a800c3b4d6d4b8ce1fd396de48a6abc7ce5051
Jacson Miranda <jacsonmiranda@gmail.com>
2020-07-17 09:09:40 -0300

    jquery code using hide(), show(),toggle(),fadeIn(), fadeOut(), fadeToggle(), slideDown(), slideUp() and slideToggle()

--- 05-01-show-hide-toggle/js/script.js
+++ 05-01-show-hide-toggle/js/script.js
@@ -2,17 +2,15 @@ $(document).ready(function() {
 	$('.item-box').on('click', 'a.info-link', function(event){
 		event.preventDefault();
 
-		//hide(), show()
+		//hide(), show(), toggle()
 		//$(this).closest('.item-box').find('.more-info').show('slow', function(){
 		//	alert('finished');
 		//});
 
-		//toggle()
-		//$(this).closest('.item-box').find('.more-info').toggle('slow', function(){
-		//	alert('finished');
-		//});
+		//fadeIn(), fadeOut(), fadeToggle()
+		//$(this).closest('.item-box').find('.more-info') .fadeToggle('slow');
 
-
+		//slideDown(), slideUp(), slideToggle()
 		$(this).closest('.item-box').find('.more-info') .slideToggle('slow');
 	});
 });
